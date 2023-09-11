a7391fc558176390b9b07671775a596adde85c5b
Amit Kumar Sutradhar <amit596450@gmail.com>
2023-09-12 04:41:01 +0600

    Product details page build

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'Home']) ->name('home');
+Route::get('/', [HomeController::class, 'home']) ->name('home');
+Route::get('/product-details', [HomeController::class, 'productDetails']) ->name('product-details');
